d37b05a435fe3ba7d7499b4242f8ba7360a80317
Tony Findeisen <github@it-ony.de>
2013-08-06 03:03:47 +0200

    working version

--- public/try/TryClass.js
+++ public/try/TryClass.js
@@ -1,7 +1,5 @@
-define(["js/core/Application", "underscore", "js/core/List", "raw!try/templates/start.html", "json!config.json", "raw!try/templates/App.xml", "raw!try/templates/AppClass.js", "try/model/File"], function(Application, _, List, start, config, AppTemplate, AppClassTemplate, File) {
+define(["js/core/Application", "underscore", "js/core/List", "raw!try/templates/start.html", "raw!try/templates/config.json", "raw!try/templates/App.xml", "raw!try/templates/AppClass.js", "try/model/File"], function(Application, _, List, start, config, AppTemplate, AppClassTemplate, File) {
 
-    var realNamespaces = ["js", "try"],
-        pluginStripper = /^([^!]+!)?(.*)$/;
 
     return Application.inherit({
 
@@ -47,24 +45,24 @@ define(["js/core/Application", "underscore", "js/core/List", "raw!try/templates/
                 runConfig,
                 files = this.$.files;
 
-            runConfig = _.clone(config);
-            runConfig.paths.virtual = "try/plugin/virtual";
-            runConfig.context = "try";
+            runConfig = JSON.parse(config);
+//            runConfig.context = "try";
 
             files.each(function(file) {
+                var path = file.$.path.replace(/\.[^.]+$/, "");
+
                 if (file.isXaml()) {
                     runConfig.xamlClasses.push(file.$.path.replace(/\.xml$/i, ""));
+                    runConfig.paths[path] = "data:" + file.$.content;
+                } else {
+                    runConfig.paths[path] = "data:text/javascript," + file.$.content + "\n//";
                 }
+
             });
 
             if (doc.document) {
                 doc = doc.document;
             }
-
-            doc.open();
-            doc.write(start);
-            doc.close();
-
 
             wnd.window.getVirtualFile = function (path) {
 
@@ -78,41 +76,20 @@ define(["js/core/Application", "underscore", "js/core/List", "raw!try/templates/
                 throw new Error("File '" + path + "' not found.");
             };
 
+            doc.open();
+            doc.write(start);
+            doc.close();
+
             wnd.onload = function() {
 
-                // overwrite define function
-                var originalDefine = wnd.define;
 
-                wnd.define = function (dependencies, fn, errFn) {
-                    dependencies = dependencies || [];
-
-                    var newDependencies = [];
-                    for (var i = 0; i < dependencies.length; i++) {
-                        var dependency = dependencies[i],
-                            match = pluginStripper.exec(dependency || ""),
-                            dependencyPath = match[2] || "",
-                            dependencyNamespace = dependencyPath.split("/").shift();
-
-                        if (_.indexOf(realNamespaces, dependencyNamespace) === -1 &&
-                            dependency !== "require" &&
-                            dependency !== "rAppid" &&
-                            !runConfig.paths.hasOwnProperty(dependency)) {
-                            // virtual file
-                            newDependencies.push("virtual!" + dependency);
-                        } else {
-                            newDependencies.push(dependency)
-                        }
-                    }
-
-                    console.log(newDependencies);
-                    originalDefine(newDependencies, fn, errFn);
-                };
-
-                wnd.window.rAppid.bootStrap("virtual!xaml!app/App.xml", null, null, runConfig, function (err, application) {
-                    console.log(err, application);
+                console.log(JSON.stringify(runConfig));
+                wnd.window.rAppid.bootStrap("app/App.xml", null, null, runConfig, function (err) {
+                    err && console.error(err);
                 });
+            }
 
-            };
+
 
         }
 
